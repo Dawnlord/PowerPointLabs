002351695ec7a50104681221a837de1382be80ea
lhy <290233090lhy@gmail.com>
2016-08-19 17:03:14 +0800

    The skeleton of log system

---
+++ PowerPointLabs/PowerPointLabs/LogSystem/AutoLogCallHandlerAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.Unity.InterceptionExtension;
+using Microsoft.Practices.EnterpriseLibrary.Logging;
+using System.Diagnostics;
+using Microsoft.Practices.EnterpriseLibrary.Common.Configuration;
+
+namespace AutoLog
+{
+
+    public class AutoLogCallHandlerAttribute : HandlerAttribute
+    {
+
+        public override ICallHandler CreateHandler(Microsoft.Practices.Unity.IUnityContainer container)
+        {
+            return new AutoLogCallHandler() { Order = this.Order };
+        }
+    }
+}
---
+++ PowerPointLabs/PowerPointLabs/LogSystem/AutoLogEmployee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.Unity;
+
+namespace AutoLog
+{
+
+    public class Employee : MarshalByRefObject
+    {
+        public Employee() { }
+
+        public string Name { get; set; }
+
+        //[AutoLogCallHandler()]
+        public void Work()
+        {
+            Console.WriteLine("Now is {0},{1} is working hard!", DateTime.Now.ToShortTimeString(), Name);
+            throw new Exception("Customer Exception");
+        }
+
+        //[AutoLogCallHandler()]
+        public override string ToString()
+        {
+            return string.Format("This is {0}.", Name);
+        }
+    }
+}
